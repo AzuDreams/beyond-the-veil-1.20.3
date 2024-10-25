c6e97df8da5f971bc6a601ebcc93bac0c965f31c
AzuDreams <bensilver2003@gmail.com>
2024-10-25 14:00:29 -0700

    Initial commit

---
+++ src/main/java/net/azu/beyondtheveil/BeyondTheVeilClient.java
@@ -0,0 +1,10 @@
+package net.azu.beyondtheveil;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BeyondTheVeilClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/azu/beyondtheveil/BeyondTheVeilDataGenerator.java
@@ -0,0 +1,11 @@
+package net.azu.beyondtheveil;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BeyondTheVeilDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
